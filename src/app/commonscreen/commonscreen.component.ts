c601346021fa34319482bb854703574753d70180
ayushi <ayushi>
2020-07-23 13:22:55 +0530

    fixedissues

--- src/app/commonscreen/commonscreen.component.ts
+++ src/app/commonscreen/commonscreen.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-commonscreen',
-  templateUrl: './commonscreen.component.html',
-  styleUrls: ['./commonscreen.component.css']
+  selector: "app-commonscreen",
+  templateUrl: "./commonscreen.component.html",
+  styleUrls: ["./commonscreen.component.css"],
 })
 export class CommonscreenComponent implements OnInit {
+  show: any = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  showSearch() {
+    console.log("asd");
+    this.show = !this.show;
   }
-
 }
--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-media',
-  templateUrl: './media.component.html',
-  styleUrls: ['./media.component.css']
+  selector: "app-media",
+  templateUrl: "./media.component.html",
+  styleUrls: ["./media.component.css"],
 })
 export class MediaComponent implements OnInit {
+  show: any = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  showSearch() {
+    console.log("asd");
+    this.show = !this.show;
   }
-
 }
